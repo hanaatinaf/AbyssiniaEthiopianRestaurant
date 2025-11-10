e8d206c971a104e1a7e95eba7610c6b415ef6cee
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-10 14:46:18 -0700

    update

---
+++ src/main/java/com/plursalsight/Product.java
@@ -0,0 +1,37 @@
+package com.plursalsight;
+
+public abstract class Product  implements  Priceable{
+    // private fields with protected accessors
+
+    private String name;
+    private  Size size;
+
+    public Product(Size size, String name) {
+        this.size = size;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    // abstraction method to be implemnted by subclass
+    public abstract  double calculatePrice();
+
+    @Override
+    public String toString(){
+        return String.format(("%s (%s)") , name ,size.getDisplayName());
+    }
+}
--- src/main/java/com/plursalsight/Topping.java
+++ src/main/java/com/plursalsight/Topping.java
@@ -1,4 +1,52 @@
 package com.plursalsight;
 
-public class Topping {
-}
+public abstract  class Topping {
+    // protected fields for sub access
+    protected String name;
+    protected  double priceSmall;
+    protected  double priceMedium;
+    protected  double priceLarge;
+
+    public Topping(String name, double priceSmall, double priceMedium, double priceLarge) {
+        this.name = name;
+        this.priceSmall = priceSmall;
+        this.priceMedium = priceMedium;
+        this.priceLarge = priceLarge;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice(Size size){
+        switch (size){
+            case SMALL:
+                return  priceSmall;
+            case MEDIUM :
+                return  priceMedium;
+            case LARGE:
+                return  priceLarge;
+            default:
+                return  0.0;
+        }
+    }
+    public void setPrice(Size size, double price ){
+        switch (size){
+            case SMALL:
+                priceSmall = price;
+                break;
+            case MEDIUM :
+                priceMedium = price;
+                break;
+
+            case LARGE:
+                priceLarge = price;
+                break;
+
+    }
+
+}}
