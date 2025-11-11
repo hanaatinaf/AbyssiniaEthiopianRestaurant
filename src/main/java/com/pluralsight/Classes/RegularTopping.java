06a3a5f3823db57d8ddb495f5d62daa52f4296d4
Hana Atinaf <hanaatianf2@gmail.com>
2025-11-11 16:11:04 -0700

    update

---
+++ src/main/java/com/pluralsight/Classes/RegularTopping.java
@@ -0,0 +1,9 @@
+package com.pluralsight.Classes;
+
+import com.pluralsight.Abstract.Topping;
+
+public class RegularTopping extends Topping {
+    public RegularTopping(String name, double priceSmall, double priceMedium, double priceLarge) {
+        super(name, priceSmall, priceMedium, priceLarge);
+    }
+}
